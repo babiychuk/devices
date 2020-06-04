1da9a480e1ef5c2d14201408cca72c8c7494d1b2
Евгений Бабийчук <e.babiychuk.ua@gmail.com>
2020-06-05 00:16:23 +0300

    Updates

---
+++ precache-manifest.9a92ef66d8458706c61e3b8e6f8e57a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac77f647e8681954099deba48c5ffc2f",
+    "url": "/devices/index.html"
+  },
+  {
+    "revision": "65fe1ccc954042c0b1a4",
+    "url": "/devices/static/js/2.f8e3b30f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/devices/static/js/2.f8e3b30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a396ca4bf9e6251bc2",
+    "url": "/devices/static/js/main.2cde196b.chunk.js"
+  },
+  {
+    "revision": "b2dd0002168c650ea6cb",
+    "url": "/devices/static/js/runtime-main.e6fed26e.js"
+  },
+  {
+    "revision": "edc66d2bb5efe16078478b20d48a196b",
+    "url": "/devices/static/media/card-reader.edc66d2b.png"
+  },
+  {
+    "revision": "c4e0860d4f75318183482bf868d8bf01",
+    "url": "/devices/static/media/cash-acceptor.c4e0860d.png"
+  },
+  {
+    "revision": "6742d9f0ccc6e497e2f7d61a088a6212",
+    "url": "/devices/static/media/cash-dispenser.6742d9f0.png"
+  },
+  {
+    "revision": "c09b5e28f754dd34fcf71ee5948fbf78",
+    "url": "/devices/static/media/pos.c09b5e28.png"
+  },
+  {
+    "revision": "978bcd8a7b1ad50d920ee3802fd3cc27",
+    "url": "/devices/static/media/printer.978bcd8a.png"
+  },
+  {
+    "revision": "e6dd0926ce5e1d4bcf051a5b557beedd",
+    "url": "/devices/static/media/product-dispenser.e6dd0926.png"
+  }
+]);
